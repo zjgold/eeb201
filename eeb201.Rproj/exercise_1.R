7af01ad465641efba4deb1d2798b0446683c1a27
zjgold <zack.j.gold@gmail.com>
2015-09-16 15:58:51 -0700

    add additional exercises

--- eeb201.Rproj/exercise_1.R
+++ eeb201.Rproj/exercise_1.R
@@ -10,3 +10,32 @@ for(ii in 1:9){
   }
 
 
+#E2 modified for loop
+
+for(ii in 1:10){
+    cat("*","&",sep="")}
+
+#E3
+#dogs final value is equal to 15, 
+#T1: 10+1=11 
+#T2:11+1=12 
+#T3:12+1=13 
+#T4:13+1=14 
+#T5:14+1=15
+
+#meatloaf prints 
+#T1: 0-5+1=-4
+#T2:-4-6+1=-9
+#T3:-9-7+1=-15
+#T4:-15-8+1=-22
+#T5:-22-9+1=-30
+
+#bubbles
+#T1: bubbles is se to -1
+#T4:-2
+#T5:-3
+#T6:-4
+
+
+
+
